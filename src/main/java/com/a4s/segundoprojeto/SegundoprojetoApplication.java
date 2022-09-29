5fa8a50a3cc278d0d46cdf748545bfd5226471eb
robinsonmourao <49078615+robinsonmourao@users.noreply.github.com>
2022-09-29 13:46:58 -0300

    Initial commit

---
+++ src/main/java/com/a4s/segundoprojeto/SegundoprojetoApplication.java
@@ -0,0 +1,13 @@
+package com.a4s.segundoprojeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegundoprojetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegundoprojetoApplication.class, args);
+	}
+
+}
